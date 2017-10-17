b8d2f6751cf36a1fabfd9705a44896f8b5e63db8
KingCastorius <malizakzorz@gmail.com>
2017-10-17 16:15:32 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import question from './components/Question';
+import quizQuestions from './api/quizQuestions';
+import Quiz from './components/Quiz'
+
+class App extends Component {
+   constructor(props) {
+    super(props);
+
+    this.state = {
+     counter: 0,
+     questionId: 1,
+     question: '',
+     answerOptions: [],
+     answer: '',
+     answersCount: {
+       nintendo: 0,
+       microsoft: 0,
+       sony: 0
+     },
+     result: ''
+    };
+    this.handleAnswerSelected = this.handleAnswerSelected.bind(this);
+  }
+
+componentWillMount() {
+   const shuffledAnswerOptions = quizQuestions.map((question) => this.shuffleArray(question.answers));  
+
+  this.setState({
+     question: quizQuestions[0].question,
+     answerOptions: shuffledAnswerOptions[0]
+   });
+ }
+
+  shuffleArray(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Quizzle formah nizzle</h2>
+        </div>
+        <Quiz
+          answer={this.state.answer}
+          answerOptions={this.state.answerOptions}
+          questionId={this.state.questionId}
+          question={this.state.question}
+          questionTotal={quizQuestions.length}
+          onAnswerSelected={this.handleAnswerSelected} />        
+      </div>
+    );
+  }
+}
+
+export default App;
